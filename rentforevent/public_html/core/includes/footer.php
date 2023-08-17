4a9f9425f1fb9323e6d87720f7f6881027335ee8
Noah <noah.hoogenboom@xs4all.nl>
2023-08-17 18:29:39 +0200

    event foto terug

--- rentforevent/public_html/core/includes/footer.php
+++ rentforevent/public_html/core/includes/footer.php
@@ -1,59 +1,71 @@
 <div class="container-fluid bg-light">
-<div class="container">
-  <footer class="py-5 mt-5">
-    <div class="row">
-      <div class="col-5 col-md-3 mb-3">
-        <h5>Service</h5>
-        <ul class="nav flex-column">     
-          <li class="nav-item mb-2"><a href="/klantenservice/openingstijden/" class="nav-link p-0 text-muted">Openingstijden</a></li>
-          <li class="nav-item mb-2"><a href="/klantenservice/afhalen/" class="nav-link p-0 text-muted">Afhalen</a></li>
-          <li class="nav-item mb-2"><a href="/klantenservice/transport" class="nav-link p-0 text-muted">Transport</a></li>
-          <li class="nav-item mb-2"><a href="/klantenservice/bestellen" class="nav-link p-0 text-muted">Bestellen</a></li>
-          <li class="nav-item mb-2"><a href="/klantenservice/betalen" class="nav-link p-0 text-muted">Betalen</a></li>
-          <li class="nav-item mb-2"><a href="/klantenservice/retourneren" class="nav-link p-0 text-muted">Retourneren</a></li>
-          <li class="nav-item mb-2"><a href="/klantenservice/bestellen" class="nav-link p-0 text-muted">Spoed- en Bijbestellingen</a></li>
-        </ul>
-      </div>
+  <div class="container">
+    <footer class="py-5 mt-5">
+      <div class="row">
+        <div class="col-5 col-md-3 mb-3">
+          <h5>Service</h5>
+          <ul class="nav flex-column">
+            <li class="nav-item mb-2"><a href="/klantenservice/openingstijden/"
+                class="nav-link p-0 text-muted">Openingstijden</a></li>
+            <li class="nav-item mb-2"><a href="/klantenservice/afhalen/" class="nav-link p-0 text-muted">Afhalen</a>
+            </li>
+            <li class="nav-item mb-2"><a href="/klantenservice/transport" class="nav-link p-0 text-muted">Transport</a>
+            </li>
+            <li class="nav-item mb-2"><a href="/klantenservice/bestellen" class="nav-link p-0 text-muted">Bestellen</a>
+            </li>
+            <li class="nav-item mb-2"><a href="/klantenservice/betalen" class="nav-link p-0 text-muted">Betalen</a></li>
+            <li class="nav-item mb-2"><a href="/klantenservice/retourneren"
+                class="nav-link p-0 text-muted">Retourneren</a></li>
+            <li class="nav-item mb-2"><a href="/klantenservice/bestellen" class="nav-link p-0 text-muted">Spoed- en
+                Bijbestellingen</a></li>
+          </ul>
+        </div>
 
-      <div class="col-5 col-md-3 mb-3">
-        <h5>Informatie</h5>
-        <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="/algemene-info/" class="nav-link p-0 text-muted">Algemene Voorwaarden</a></li>
-          <li class="nav-item mb-2"><a href="/algemene-info/" class="nav-link p-0 text-muted">Privacyverklaring</a></li>
-        </ul>
-      </div>
-    <div class="col-5 col-md-3 mb-3">
-        <h5>Social</h5>
-        <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="https://facebook.com/eventsolutions.nu" target="_blank" class="nav-link p-0 text-muted">Volg ons op facebook</a></li>
-          <li class="nav-item mb-2"><a href="https://instagram.com/eventsolutions_nl" target="_blank" class="nav-link p-0 text-muted">Volg ons op instagram</a></li>
-          <li class="nav-item mb-2"><a href="https://wa.me/31262022943" target="_blank" class="nav-link p-0 text-muted">WhatsApp ons</a></li>
-        </ul>
-    </div>
-   
-      <div class="col-5 col-md-3 mb-3">
-        <h5>Contact</h5>
-        <ul class="nav flex-column">
-          <li class="nav-item mb-2">
-            Doctor Moonsweg 5<br />
-            Unit 66<br />
-            5437 BG Beers (NB)
-            <br /><br />
-            0485 - 799 815<br />
-            verhuur@rentforevents.nl</li>
-          <li class="nav-item mb-2"></li>
-          <li class="nav-item mb-2">
+        <div class="col-5 col-md-3 mb-3">
+          <h5>Informatie</h5>
+          <ul class="nav flex-column">
+            <li class="nav-item mb-2"><a href="/algemene-info/" class="nav-link p-0 text-muted">Algemene Voorwaarden</a>
+            </li>
+            <li class="nav-item mb-2"><a href="/algemene-info/" class="nav-link p-0 text-muted">Privacyverklaring</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-5 col-md-3 mb-3">
+          <h5>Social</h5>
+          <ul class="nav flex-column">
+            <li class="nav-item mb-2"><a href="https://facebook.com/eventsolutions.nu" target="_blank"
+                class="nav-link p-0 text-muted">Volg ons op facebook</a></li>
+            <li class="nav-item mb-2"><a href="https://instagram.com/eventsolutions_nl" target="_blank"
+                class="nav-link p-0 text-muted">Volg ons op instagram</a></li>
+            <li class="nav-item mb-2"><a href="https://wa.me/31262022943" target="_blank"
+                class="nav-link p-0 text-muted">WhatsApp ons</a></li>
+          </ul>
+        </div>
+
+        <div class="col-5 col-md-3 mb-3">
+          <h5>Contact</h5>
+          <ul class="nav flex-column">
+            <li class="nav-item mb-2">
+              Doctor Moonsweg 5<br />
+              Unit 66<br />
+              5437 BG Beers (NB)
+              <br /><br />
+              0485 - 799 815<br />
+              verhuur@rentforevents.nl
+            </li>
+            <li class="nav-item mb-2"></li>
+            <li class="nav-item mb-2">
               KvK 58496637<br />
               BTW NL002280072B66
-          </li>
-        </ul>
-      </div>
-    </div>
-    <div class="flex-sm-row py-4 my-4 border-top ">
-        <div class="float-end">
-            <img src="img/eventsolutions.png" style="height:35px;" alt="EventSolutions" />
+            </li>
+          </ul>
         </div>
-    </div>
-  </footer>
-</div>
+      </div>
+      <div class="flex-sm-row py-4 my-4 border-top ">
+        <div class="float-end">
+          <img src="images/eventsolutions.png" style="height:35px;" alt="EventSolutions" />
+        </div>
+      </div>
+    </footer>
+  </div>
 </div>
