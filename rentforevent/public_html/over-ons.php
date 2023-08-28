e78f8fbab7484268b428976823a20557054b1a8b
Noah <noah.hoogenboom@xs4all.nl>
2023-08-28 17:45:12 +0200

    new pages: assoritment, contact, inspiratie overon

---
+++ rentforevent/public_html/over-ons.php
@@ -0,0 +1,22 @@
+<?php
+require_once './core/system.init.php';
+
+$items = api::Call("artikelgroepen/voorpagina");
+?>
+
+<!doctype html>
+<html lang="en">
+<?php require_once header; ?>
+
+<body>
+    <?php require_once navigation; ?>
+    <div class="container">
+        <div class="row text-center">
+            <h3>Wie zijn wij?</h3>
+        </div>
+    </div>
+    <?php require_once footer; ?>
+    <?php require_once scripts; ?>
+</body>
+
+</html>
